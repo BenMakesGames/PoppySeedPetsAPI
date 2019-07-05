1d4369b571c291ba1278a33662a6b6c658ec7bb5
Ben H <ben@benmakesgames.com>
2019-07-04 22:39:56 -0400

    migration fix

--- src/Migrations/Version20190705022950.php
+++ src/Migrations/Version20190705022950.php
@@ -22,7 +22,6 @@ final class Version20190705022950 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE inventory ADD CONSTRAINT FK_B12D4A367E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE museum_item CHANGE created_by_id created_by_id INT DEFAULT NULL');
     }
 
@@ -31,7 +30,6 @@ final class Version20190705022950 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE inventory DROP FOREIGN KEY FK_B12D4A367E3C61F9');
         $this->addSql('ALTER TABLE museum_item CHANGE created_by_id created_by_id INT NOT NULL');
     }
 }
