c553db4a05df710fc7c6a623ccbb4eabd06bc2f3
Ben H <ben@benmakesgames.com>
2019-07-07 10:20:01 -0400

    max pets => 2

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -103,7 +103,7 @@ class User implements UserInterface
      * @ORM\Column(type="integer")
      * @Groups({"myAccount"})
      */
-    private $maxPets = 1;
+    private $maxPets = 2;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\UserFriend", mappedBy="user", orphanRemoval=true)
