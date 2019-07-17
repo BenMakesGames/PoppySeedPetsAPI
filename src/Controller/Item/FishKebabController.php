e6b8bd3fe8c080d96a2f87c502cfee4ca4166483
Ben H <ben@benmakesgames.com>
2019-07-17 16:28:43 -0400

    fishkebabs and sand dollars

---
+++ src/Controller/Item/FishKebabController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller\Item;
+
+use App\Entity\Inventory;
+use App\Service\InventoryService;
+use App\Service\ResponseService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/item/fishKebab")
+ */
+class FishKebabController extends PsyPetsItemController
+{
+    /**
+     * @Route("/{inventory}/takeApart", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function read(
+        Inventory $inventory, ResponseService $responseService, InventoryService $inventoryService,
+        EntityManagerInterface $em
+    )
+    {
+        $this->validateInventory($inventory, 'fishKebab/#/takeApart');
+
+        $user = $this->getUser();
+
+        $inventoryService->receiveItem('Fish', $user, $user, $user->getName() . ' got this from a Fishkebab.');
+        $inventoryService->receiveItem('Fish', $user, $user, $user->getName() . ' got this from a Fishkebab.');
+        $inventoryService->receiveItem('Fish', $user, $user, $user->getName() . ' got this from a Fishkebab.');
+        $inventoryService->receiveItem('Crooked Stick', $user, $user, $user->getName() . ' got this from a Fishkebab.');
+
+        $em->remove($inventory);
+
+        $em->flush();
+
+        return $responseService->itemActionSuccess('You take the Fishkebab apart, receiving three pieces of Fish, and a Crooked Stick.', [ 'reloadInventory' => true, 'itemDeleted' => true ]);
+    }
+}
---
+++ src/Controller/Item/SandDollarController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Controller\Item;
+
+use App\Entity\Inventory;
+use App\Service\InventoryService;
+use App\Service\ResponseService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/item/sandDollar")
+ */
+class SandDollarController extends PsyPetsItemController
+{
+    /**
+     * @Route("/{inventory}/loot", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function read(
+        Inventory $inventory, ResponseService $responseService, InventoryService $inventoryService,
+        EntityManagerInterface $em
+    )
+    {
+        $this->validateInventory($inventory, 'sandDollar/#/loot');
+
+        $user = $this->getUser();
+
+        $user->increaseMoneys(1);
+
+        $inventoryService->receiveItem('Silica Grounds', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+
+        if(mt_rand(1, 10) === 1)
+        {
+            $inventoryService->receiveItem('String', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds - and oh! What\'s this? Oh, it\'s a soggy bit of String. Well, it\'ll dry out, eventually.';
+        }
+        else if(mt_rand(1, 10) === 1)
+        {
+            $inventoryService->receiveItem('Talon', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds - and hey: there\'s something else in here! It\'s a shark tooth, maybe? Or, like, a claw? Maybe a Talon? Let\'s go with Talon.';
+        }
+        else if(mt_rand(1, 20) === 1)
+        {
+            $inventoryService->receiveItem('Mermaid Egg', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds - and oh! There\'s something squishy! Ah! It\'s a Mermaid Egg!';
+        }
+        else if(mt_rand(1, 20) === 1)
+        {
+            $inventoryService->receiveItem('Plastic', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds - and what? There\'s some... Plastic in here?? That\'s kind of sad :| Well... one less piece in the ocean, I guess...';
+        }
+        else if(mt_rand(1, 50) === 1)
+        {
+            $inventoryService->receiveItem('Iron Key', $user, $user, $user->getName() . ' found this inside a Sand Dollar.');
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds - and oh! What\'s this? Some kind of key! An Iron Key!';
+        }
+        else
+        {
+            $message = 'You rummage around inside the Sand Dollar. There was 1 dollar - er, I mean, moneys - and also a bit of sand - er, I mean, Silica Grounds.';
+        }
+
+        $em->remove($inventory);
+
+        $em->flush();
+
+        return $responseService->itemActionSuccess($message, [ 'reloadInventory' => true, 'itemDeleted' => true ]);
+    }
+}
