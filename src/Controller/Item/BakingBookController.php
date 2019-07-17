37b41ac86d9d6c8baf0b20472fa068bab22cb1a9
Ben H <ben@benmakesgames.com>
2019-07-17 19:37:23 -0400

    big book of baking!

---
+++ src/Controller/Item/BakingBookController.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Controller\Item;
+
+use App\Entity\Inventory;
+use App\Service\ResponseService;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/item/bakingBook")
+ */
+class BakingBookController extends PsyPetsItemController
+{
+    /**
+     * @Route("/{inventory}/read", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function read(Inventory $inventory, ResponseService $responseService)
+    {
+        $this->validateInventory($inventory, 'bakingBook/#/read');
+
+        return $responseService->itemActionSuccess('# Big Book of Baking
+
+## Table of Contents
+
+#### Basic Ingredients
+
+* Baking Powder
+* Baking Soda
+* Butter
+* Cream of Tartar
+* Wheat Flour
+
+#### Recipes
+
+* Meringues
+* Pancakes
+* Plain Bread
+* Sugar Cookies (World\'s Best!)
+
+## Basic Ingredients
+
+#### Baking Powder
+
+Combine an equal measure of Baking Soda and Cream of Tartar.
+
+#### Baking Soda
+
+Dissolve a block of Limestone. You will get about 10 batches of Baking Soda.
+
+#### Butter
+
+Churn a bottle of Creamy Milk to create Butter.
+
+#### Cream of Tartar
+
+Can be found as a byproduct of wine-making. (As an example, you can age some Blueberries or Blackberries to make wine, and some Cream of Tartar.) 
+
+#### Wheat Flour
+
+Simply grind some Wheat into a fine powder.
+
+## Recipes
+
+#### Meringues
+
+* Sugar
+* Egg
+* Cream of Tartar
+
+Optionally, add Cocoa Powder for Chocolate Meringues!
+
+#### Pancakes
+
+* Baking Soda
+* Butter
+* Creamy Milk
+* Egg
+* Sugar
+* Wheat Flour
+
+The milk provides enough acidity for this recipe, so Cream of Tartar (or Baking Powder) is not required; Baking Soda on its own will do!
+
+#### Plain Bread
+
+* Baker\'s Yeast
+* Butter
+* Creamy Milk
+* Sugar
+* Wheat Flour
+
+#### Sugar Cookies (World\'s Best!)
+
+* Baking Powder
+* Butter
+* Egg
+* Sugar
+* Wheat Flour
+
+If you have Baking Soda and Cream of Tartar lying around, but no Baking Powder, feel free to substitute the Baking Powder here with Baking Soda and Cream of Tartar directly.
+');
+    }
+}
