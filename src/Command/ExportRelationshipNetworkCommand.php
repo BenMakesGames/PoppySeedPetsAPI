5a5cd3bce4ff94913243e95d4ab8a40abf9b3ce8
Ben H <ben@benmakesgames.com>
2019-10-21 02:25:16 -0400

    export relationship data

---
+++ src/Command/ExportRelationshipNetworkCommand.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Command;
+
+use App\Repository\PetRelationshipRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportRelationshipNetworkCommand extends Command
+{
+    private $em;
+    private $petRelationshipRepository;
+
+    public function __construct(EntityManagerInterface $em, PetRelationshipRepository $petRelationshipRepository)
+    {
+        $this->em = $em;
+        $this->petRelationshipRepository = $petRelationshipRepository;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:export-relationship-network')
+            ->setDescription('Export relationship data in a format appropriate for https://www.d3-graph-gallery.com/graph/network_basic.html')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $relationships = $this->petRelationshipRepository->findAll();
+
+        $pets = [];
+        $links = [];
+
+        foreach($relationships as $relationship)
+        {
+            $pet = $relationship->getPet();
+            $pets[$pet->getId()] = [
+                'id' => $pet->getId(),
+                'owner' => $pet->getOwner()->getId(),
+                'name' => $pet->getName() . ' #' . $pet->getId()
+            ];
+
+            $links[] = [
+                'source' => $pet->getId(),
+                'target' => $relationship->getRelationship()->getId(),
+            ];
+        }
+
+        $pets = array_values($pets);
+
+        echo \GuzzleHttp\json_encode([
+            'links' => $links,
+            'nodes' => $pets
+        ]);
+    }
+}
