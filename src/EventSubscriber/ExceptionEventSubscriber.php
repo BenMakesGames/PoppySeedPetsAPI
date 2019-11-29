0933174438ff62c24f20115520f8d142e80695aa
Ben H <ben@benmakesgames.com>
2019-11-29 00:11:09 -0500

    catch EntityNotFoundException

--- src/EventSubscriber/ExceptionEventSubscriber.php
+++ src/EventSubscriber/ExceptionEventSubscriber.php
@@ -2,7 +2,9 @@
 namespace App\EventSubscriber;
 
 use App\Service\ResponseService;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -32,6 +34,20 @@ class ExceptionEventSubscriber implements EventSubscriberInterface
             $event->setResponse($this->responseService->error(
                 $e->getStatusCode(),
                 [ $e->getMessage() ]
+            ));
+        }
+        else if($e instanceof EntityNotFoundException)
+        {
+            if(strpos($e->getMessage(), 'App\\Entity\\Inventory') !== false)
+                $message = 'That item doesn\'t exist... weird. Maybe it got used up? Reload and try again.';
+            else if(strpos($e->getMessage(), 'App\\Entity\\Pet') !== false)
+                $message = 'That pet doesn\'t exist... weird. Reload and try again?';
+            else
+                $message = 'The thing you were trying to interact with doesn\'t exist! That generally shouldn\'t happen... reload and try again?';
+
+            $event->setResponse($this->responseService->error(
+                Response::HTTP_NOT_FOUND,
+                [ $message ]
             ));
         }
     }
