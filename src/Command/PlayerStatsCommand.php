372b7224a1d6f4547dd729412ea1135ffbca200a
Ben H <ben@benmakesgames.com>
2020-01-20 21:32:11 -0500

    player-stats fix (3 day stats were accidentally reporting on 1.5 days, and not 3)

--- src/Command/PlayerStatsCommand.php
+++ src/Command/PlayerStatsCommand.php
@@ -33,14 +33,14 @@ class PlayerStatsCommand extends PoppySeedPetsCommand
         $data = [
             'Activity in the Last 1 Day' => $this->getCount($activityQuery, $now->modify('-24 hours')->format('Y-m-d H:i:s')),
             'Activity in the Last 2 Days' => $this->getCount($activityQuery, $now->modify('-48 hours')->format('Y-m-d H:i:s')),
-            'Activity in the Last 3 Days' => $this->getCount($activityQuery, $now->modify('-36 hours')->format('Y-m-d H:i:s')),
+            'Activity in the Last 3 Days' => $this->getCount($activityQuery, $now->modify('-72 hours')->format('Y-m-d H:i:s')),
             'Activity in the last 5 Days' => $this->getCount($activityQuery, $now->modify('-5 days')->format('Y-m-d H:i:s')),
             'Activity in the last 1 Week' => $this->getCount($activityQuery, $now->modify('-7 days')->format('Y-m-d H:i:s')),
             'Activity in the last 2 Weeks' => $this->getCount($activityQuery, $now->modify('-14 days')->format('Y-m-d H:i:s')),
 
             'New Users in the Last 1 Day' => $this->getCount($newUsersQuery, $now->modify('-24 hours')->format('Y-m-d H:i:s')),
             'New Users in the Last 2 Days' => $this->getCount($newUsersQuery, $now->modify('-48 hours')->format('Y-m-d H:i:s')),
-            'New Users in the Last 3 Days' => $this->getCount($newUsersQuery, $now->modify('-36 hours')->format('Y-m-d H:i:s')),
+            'New Users in the Last 3 Days' => $this->getCount($newUsersQuery, $now->modify('-72 hours')->format('Y-m-d H:i:s')),
             'New Users in the last 5 Days' => $this->getCount($newUsersQuery, $now->modify('-5 days')->format('Y-m-d H:i:s')),
             'New Users in the last 1 Week' => $this->getCount($newUsersQuery, $now->modify('-7 days')->format('Y-m-d H:i:s')),
             'New Users in the last 2 Weeks' => $this->getCount($newUsersQuery, $now->modify('-14 days')->format('Y-m-d H:i:s')),
