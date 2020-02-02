f705e9acd8587065d0192236a83abea90d2e933b
Ben H <ben@benmakesgames.com>
2020-02-02 00:55:54 -0500

    is this the best way to fix this? >_>

--- src/Service/Filter/PetGroupFilterService.php
+++ src/Service/Filter/PetGroupFilterService.php
@@ -63,6 +63,7 @@ class PetGroupFilterService
             $qb
                 ->andWhere('p.owner=:owner')
                 ->setParameter('owner', $value)
+                ->groupBy('g')
             ;
         }
     }
