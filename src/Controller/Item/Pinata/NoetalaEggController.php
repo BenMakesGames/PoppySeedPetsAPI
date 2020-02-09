7a67047aeae1290731aa1ac1f62bffe3572bfd30
Ben H <ben@benmakesgames.com>
2020-02-08 19:10:30 -0500

    quina-whatever dye from Noetala eggs

--- src/Controller/Item/Pinata/NoetalaEggController.php
+++ src/Controller/Item/Pinata/NoetalaEggController.php
@@ -30,22 +30,27 @@ class NoetalaEggController extends PoppySeedPetsItemController
 
         $location = $inventory->getLocation();
 
-        $inventoryService->receiveItem('Fluff', $user, $user, $user->getName() . ' peeled this off a Noetala Egg', $location);
+        $inventoryService->receiveItem('Quinacridone Magenta', $user, $user, $user->getName() . ' peeled this off a Noetala Egg', $location);
 
         // 75% chance of more Fluff
-        if(mt_rand(1, 4) != 1)
-            $inventoryService->receiveItem('Fluff', $user, $user, $user->getName() . ' peeled this off a Noetala Egg', $location);
+        $includeFluff = mt_rand(1, 4) != 1;
 
         $loot = ArrayFunctions::pick_one([
-            'Quintessence', 'Baking Soda', 'Tentacle', 'Green Dye'
+            'Quintessence', 'Talon', 'Tentacle', 'Green Dye'
         ]);
 
-        $inventoryService->receiveItem($loot, $user, $user, $user->getName() . ' harvested this from a Noetala Egg', $location);
+        if($includeFluff)
+            $inventoryService->receiveItem('Fluff', $user, $user, $user->getName() . ' peeled this off a Noetala Egg.', $location);
+
+        $inventoryService->receiveItem($loot, $user, $user, $user->getName() . ' harvested this from a Noetala Egg.', $location);
 
         $em->remove($inventory);
 
         $em->flush();
 
-        return $responseService->itemActionSuccess('You pull the clinging fibers off of the Noetala Egg, and break it open. Your reward? ' . $loot . '. (Well, and some Fluff from the fibers.)' , [ 'reloadInventory' => true, 'itemDeleted' => true ]);
+        if($includeFluff)
+            return $responseService->itemActionSuccess('You pull the clinging fibers off of the Noetala Egg, and break it open; a strange, purple liquid oozes out. Your reward? ' . $loot . '. (Well, and some Fluff from the fibers.)' , [ 'reloadInventory' => true, 'itemDeleted' => true ]);
+        else
+            return $responseService->itemActionSuccess('You break the egg open; a strange, purple liquid oozes out. Your reward? ' . $loot . '. (Well, and some Fluff from the fibers.)' , [ 'reloadInventory' => true, 'itemDeleted' => true ]);
     }
 }
