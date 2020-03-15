dd7010980887139a9e681020faa91cab248fdd5d
Ben H <ben@benmakesgames.com>
2020-03-15 19:11:50 -0400

    fixing metal detector bug...

--- src/Service/PetActivity/Crafting/ProgrammingService.php
+++ src/Service/PetActivity/Crafting/ProgrammingService.php
@@ -152,7 +152,7 @@ class ProgrammingService
 
     private function createMetalDetector(Pet $pet): PetActivityLog
     {
-        $roll = mt_rand(1, 20 + $pet->getIntelligence() + $pet->getDexterity() + max($pet->getScience() + $pet->getCrafts()));
+        $roll = mt_rand(1, 20 + $pet->getIntelligence() + $pet->getDexterity() + max($pet->getScience(), $pet->getCrafts()));
 
         if($roll <= 2)
         {
