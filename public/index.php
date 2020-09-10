fb064e8f7a87fcd865a1db09b0dda727a921e30b
Ben H <ben@benmakesgames.com>
2020-09-10 18:21:35 -0400

    maintenance mode!

--- public/index.php
+++ public/index.php
@@ -20,6 +20,17 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
+if(array_key_exists('APP_MAINTENANCE', $_SERVER) && $_SERVER['APP_MAINTENANCE'])
+{
+    header('Access-Control-Allow-Origin: *');
+    echo json_encode([
+        'success' => false,
+        'errors' => [ 'Poppy Seed Pets is getting a little software upgrade. It\'ll be back in just a feeewwww minutes!' ]
+    ]);
+
+    exit;
+}
+
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
