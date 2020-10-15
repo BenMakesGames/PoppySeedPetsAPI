25a3cd2fefdfad25afb6002eeee47084363ca397
Ben H <ben@benmakesgames.com>
2020-10-15 15:43:32 -0400

    Revert "we're doin' this: it's Symfony 5.1 time!"
    everything broke :(

--- src/EventSubscriber/ExceptionEventSubscriber.php
+++ src/EventSubscriber/ExceptionEventSubscriber.php
@@ -42,7 +42,7 @@ class ExceptionEventSubscriber implements EventSubscriberInterface
 
     public function onKernelException(ExceptionEvent $event)
     {
-        $e = $event->getThrowable();
+        $e = $event->getException();
 
         if($e instanceof HttpException)
         {
