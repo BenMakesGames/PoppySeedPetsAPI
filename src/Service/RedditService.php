be08180d1dda41b137e1019c7edf577e431bc471
Ben H <ben@benmakesgames.com>
2020-11-12 11:41:32 -0500

    more reddit integration work

--- src/Service/RedditService.php
+++ src/Service/RedditService.php
@@ -3,36 +3,22 @@ namespace App\Service;
 
 use App\Entity\Article;
 use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use kamermans\OAuth2\GrantType\ClientCredentials;
-use kamermans\OAuth2\OAuth2Middleware;
 
 class RedditService
 {
-    /** @var HandlerStack  */
-    private $clientHandler;
+    private $apiClientId;
+    private $apiClientSecret;
+    private $username;
+    private $password;
 
-    public function __construct(string $apiClientId, string $apiClientSecret)
+    public function __construct(
+        string $apiClientId, string $apiClientSecret, string $username, string $password
+    )
     {
-        $reauthClient = new Client([
-            'base_uri' => 'https://www.reddit.com/api/v1/access_token'
-        ]);
-
-        $grant = new ClientCredentials($reauthClient, [
-            'client_id' => $apiClientId,
-            'client_secret' => $apiClientSecret,
-            'grant_type' => 'client_credentials',
-        ]);
-
-        $oauth2Middleware = new OAuth2Middleware($grant);
-
-        $this->clientHandler = HandlerStack::create();
-        $this->clientHandler->push($oauth2Middleware);
-    }
-
-    private function createClient(): Client
-    {
-        return new Client([ 'handler' => $this->clientHandler, 'auth' => 'oauth' ]);
+        $this->apiClientId = $apiClientId;
+        $this->apiClientSecret = $apiClientSecret;
+        $this->username = $username;
+        $this->password = $password;
     }
 
     public function getUserAgent(): string
@@ -40,12 +26,38 @@ class RedditService
         return 'PHP:Poppy Seed Pets news poster:1 (by /u/BenMakesGames)';
     }
 
+    private function getAccessToken()
+    {
+        $client = new Client([
+            'base_uri' => 'https://www.reddit.com/'
+        ]);
+
+        $response = $client->post('api/v1/access_token', [
+            'data' => [
+                'grant_type' => 'password',
+                'username' => $this->username,
+                'password' => $this->password,
+            ],
+            'headers' => [
+                'User-Agent' => $this->getUserAgent(),
+            ],
+            'auth' => [ $this->apiClientId, $this->apiClientSecret ]
+        ]);
+
+        return $response['access_token'];
+    }
+
     public function postArticle(Article $article)
     {
-        $client = $this->createClient();
+        $accessToken = $this->getAccessToken();
 
-        $client->post('/api/submit', [
+        $client = new Client([
+            'base_uri' => 'https://oauth.reddit.com/'
+        ]);
+
+        $client->post('api/submit', [
             'headers' => [
+                'Authorization' => 'bearer ' . $accessToken,
                 'User-Agent' => $this->getUserAgent(),
             ],
             'json' => [
