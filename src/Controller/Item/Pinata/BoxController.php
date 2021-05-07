716e6b9ecdbdf58b282ecb405f576b87741fb89b
Ben H <ben@benmakesgames.com>
2021-05-07 18:52:40 -0400

    paper bag silliness

--- src/Controller/Item/Pinata/BoxController.php
+++ src/Controller/Item/Pinata/BoxController.php
@@ -650,23 +650,25 @@ class BoxController extends PoppySeedPetsItemController
      */
     public function openPaperBag(
         Inventory $inventory, ResponseService $responseService, InventoryService $inventoryService, Squirrel3 $squirrel3,
-        UserStatsRepository $userStatsRepository, EntityManagerInterface $em
+        UserStatsRepository $userStatsRepository, EntityManagerInterface $em, ItemRepository $itemRepository
     )
     {
         $user = $this->getUser();
 
         $this->validateInventory($inventory, 'box/paperBag/#/open');
 
-        $item = $squirrel3->rngNextFromArray([
+        $item = $itemRepository->findOneByName($squirrel3->rngNextFromArray([
             'Apricot', 'Yeast', 'Baking Soda', 'Beans', 'Blackberry Lassi', 'Blueberries', 'Butter', 'Celery',
             'Cockroach', 'Corn', 'Cream of Tartar', 'Creamy Milk', 'Dark Matter', 'Egg', 'Fish', 'Fluff',
             'Grandparoot', 'Honeydont', 'Hot Dog', 'Iron Ore', 'Kombucha', 'Melon Bun', 'Naner', 'Oil', 'Onion',
             'Orange', 'Pamplemousse', 'Plain Yogurt', 'Quintessence', 'Red', 'Rice', 'Seaweed', 'Secret Seashell',
             'Silica Grounds', 'Smallish Pumpkin', 'Sugar', 'Toad Legs', 'Tomato', 'Wheat Flour',
             'World\'s Best Sugar Cookie', 'Glowing Four-sided Die', 'Mint', 'Mixed Nuts', 'Canned Food',
-        ]);
+        ]));
 
-        if($item === 'Cockroach' && $squirrel3->rngNextInt(1, 3) === 1)
+        $openedStat = $userStatsRepository->incrementStat($user, 'Opened ' . $inventory->getItem()->getNameWithArticle());
+
+        if($item->getName() === 'Cockroach' && $squirrel3->rngNextInt(1, 3) === 1)
         {
             $numRoaches = $squirrel3->rngNextInt(6, 8);
 
@@ -685,10 +687,27 @@ class BoxController extends PoppySeedPetsItemController
                 ->setSpice($inventory->getSpice())
             ;
 
-            $message = 'You open the bag... ah! ' . $item . '!';
+            if($openedStat->getValue() === 7 || ($openedStat->getValue() >= 12 && $squirrel3->rngNextInt(1, 20) === 1))
+            {
+                $message = $squirrel3->rngNextFromArray([
+                    'You open the bag... WHAT THE FU-- oh, wait, nm: it\'s just ' . $item->getNameWithArticle() . '.',
+                    'Da, da, da, DAAAAAAAA: ' . $item->getNameWithArticle() . '!',
+                    'You open the bag... just some ordinary, run-of-the-mill ' . $item->getName() . '.',
+                    'You open the bag... there\'s another Paper Bag inside, so you open _that_... ah! ' . ucfirst($item->getNameWithArticle()) . '! (Finally!)',
+                    'You open the bag... ah! It\'s the ' . $item->getName() . ' you\'ve always dreamed about; the ' . $item->getName() . ' you _so richly deserve_.',
+                    'You open the bag... ah! ' . ucfirst($item->getNameWithArticle()) . ' fit for a queen! Or a king! Or whatever! You do you!',
+                    'You open the bag... it\'s empty??? Wait, no, here it is, stuck under a flap in the deepest recesses of the bag: ' . $item->getNameWithArticle() . '.',
+                    'You open the bag... you pray it\'s not ' . $item->getNameWithArticle() . ', but - and I hate to break it to you - that\'s _exactly_ what it is.',
+                    "You open the-- aw, shit! The bag tore!\n\nSomething tumbles out, and makes a very uncomfortable noise when it hit the ground. Well, at least it didn't hit you on its way there.\n\nYou look past the bag, to the floor, and at the source of your consternation. Hmph: all that trouble for " . $item->getNameWithArticle() . "...",
+                    'You open the bag... iiiiiiiiiiit\'s ' . $item->getNameWithArticle() . '.',
+                    "If I tell you there's " . $item->getNameWithArticle() . " in this bag, will you believe me?\n\nThere\'s " . $item->getNameWithArticle() . " in this bag.",
+                    "You open the bag... but it's one of those Mimic Paper Bags! OH NO! IT CHOMPS DOWN HARD ON YOUR-- oh. Wait, it... it doesn't have any teeth.\n\nWell, it's a bit more work - and a bit wetter - than you\'d like, but with a little work you manage to extract " . $item->getNameWithArticle() . ".",
+                    'You open the bag... but it\'s one of those Mimic Paper Bags! OH NO! It wriggles free, drops to the ground, and scurries off, ' . $item->getNameWithArticle() . ' tumbling out of its... mouth (???) as it goes.',
+                ]);
+            }
+            else
+                $message = 'You open the bag... ah! ' . ucfirst($item->getNameWithArticle()) . '!';
         }
-
-        $userStatsRepository->incrementStat($user, 'Opened ' . $inventory->getItem()->getNameWithArticle());
 
         $em->remove($inventory);
 
