7a161d6acbe71ae889aa332d2a5eacdf55ecc76f
Ben H <ben@benmakesgames.com>
2021-06-03 21:33:12 -0400

    anti-grav'd pets with PREHENSILE_TONGUE get a climbing bonus

--- src/Model/ComputedPetSkills.php
+++ src/Model/ComputedPetSkills.php
@@ -277,6 +277,9 @@ class ComputedPetSkills
     {
         $skill = new TotalPetSkill();
         $skill->tool = $this->pet->getTool() ? $this->pet->getTool()->climbingBonus() : 0;
+        $skill->statusEffects =
+            (($this->pet->hasMerit(MeritEnum::PREHENSILE_TONGUE) && $this->pet->hasStatusEffect(StatusEffectEnum::ANTI_GRAVD)) ? 3 : 0)
+        ;
 
         return $skill;
     }
