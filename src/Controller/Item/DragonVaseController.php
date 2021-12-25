a05ec63a30aa577c1eb035717d101a1f2ba93bd9
Ben H <ben@benmakesgames.com>
2021-12-25 16:36:33 -0500

    grammar fix!

--- src/Controller/Item/DragonVaseController.php
+++ src/Controller/Item/DragonVaseController.php
@@ -76,7 +76,7 @@ class DragonVaseController extends PoppySeedPetsItemController
             $petItem = $rng->rngNextFromArray([ 'Quintessence', 'Wings', 'Feathers' ]);
 
             if($petItem == 'Feathers')
-                $activityLog = $responseService->createActivityLog($pet, ActivityHelpers::PetName($pet) . ' Wings some ' . $petItem . ' that flew out of a Dragon Vase ' . $user->getName() . ' smashed, but accidentally reduced it to mere Feathers.', '');
+                $activityLog = $responseService->createActivityLog($pet, ActivityHelpers::PetName($pet) . ' ' . $description . ' some Wings some ' . $petItem . ' that flew out of a Dragon Vase ' . $user->getName() . ' smashed, but accidentally reduced it to mere Feathers.', '');
             else
                 $activityLog = $responseService->createActivityLog($pet, ActivityHelpers::PetName($pet) . ' ' . $description . ' some ' . $petItem . ' that flew out of a Dragon Vase ' . $user->getName() . ' smashed.', '');
 
