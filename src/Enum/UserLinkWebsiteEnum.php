1f60678e9e16fd32a19b58c2b250256d424f4864
Ben <ben@benmakesgames.com>
2023-07-15 15:01:08 -0400

    more networks to link to

--- src/Enum/UserLinkWebsiteEnum.php
+++ src/Enum/UserLinkWebsiteEnum.php
@@ -6,9 +6,17 @@ class UserLinkWebsiteEnum
 {
     use Enum;
 
-    public const POPPY_SEED_PETS = 'PSP';
-    public const YOUTUBE = 'YouTube';
-    public const TUMBLR = 'Tumblr';
-    public const STEAM = 'Steam';
     public const DEVIANT_ART = 'DeviantArt';
+    public const FLIGHT_RISING = 'FlightRising';
+    public const FUR_AFFINITY = 'FurAffinity';
+    public const DISCORD = 'Discord';
+    public const GAIA_ONLINE = 'GaiaOnline';
+    public const GITHUB = 'GitHub';
+    public const NINTENDO = 'Nintendo';
+    public const POPPY_SEED_PETS = 'PSP';
+    public const INSTAGRAM = 'Instagram';
+    public const STEAM = 'Steam';
+    public const TUMBLR = 'Tumblr';
+    public const TWITCH = 'Twitch';
+    public const YOUTUBE = 'YouTube';
 }
