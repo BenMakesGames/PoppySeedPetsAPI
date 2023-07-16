3c92a6d3090045839e7f49f7a0fa1a8efd887a0c
Ben H <ben@benmakesgames.com>
2023-07-16 17:55:29 -0400

    added two more sites for user links

--- src/Enum/UserLinkWebsiteEnum.php
+++ src/Enum/UserLinkWebsiteEnum.php
@@ -6,12 +6,14 @@ class UserLinkWebsiteEnum
 {
     use Enum;
 
+    public const COHOST = 'Cohost';
     public const DEVIANT_ART = 'DeviantArt';
     public const FLIGHT_RISING = 'FlightRising';
     public const FUR_AFFINITY = 'FurAffinity';
     public const GAIA_ONLINE = 'GaiaOnline';
     public const GITHUB = 'GitHub';
     public const NINTENDO = 'Nintendo';
+    public const PIXEL_CATS_END = 'PixelCatsEnd';
     public const POPPY_SEED_PETS = 'PSP';
     public const INSTAGRAM = 'Instagram';
     public const STEAM = 'Steam';
