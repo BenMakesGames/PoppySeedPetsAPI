3d6ed56e0c1efc11a7d7e23d472a1229e1ef82d6
Ben H <ben@benmakesgames.com>
2023-08-07 16:30:06 -0400

    yellowy lime sticker

---
+++ src/Controller/Item/Book/YellowyLimeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Item\Book;
+
+use App\Controller\Item\ItemControllerHelpers;
+use App\Entity\Inventory;
+use App\Entity\User;
+use App\Enum\LocationEnum;
+use App\Exceptions\PSPInvalidOperationException;
+use App\Repository\InventoryRepository;
+use App\Service\ResponseService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/item/yellowyLime")
+ */
+class YellowyLimeController extends AbstractController
+{
+    /**
+     * @Route("/{inventory}/read", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     */
+    public function read(
+        Inventory $inventory, ResponseService $responseService,
+        InventoryRepository $inventoryRepository
+    )
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        ItemControllerHelpers::validateInventory($user, $inventory, 'yellowyLime/#/read');
+
+        $magnifyingGlass = $inventoryRepository->findAnyOneFromItemGroup($user, 'Magnifying Glass', [
+            LocationEnum::HOME,
+            LocationEnum::BASEMENT,
+            LocationEnum::MANTLE,
+            LocationEnum::WARDROBE,
+        ]);
+
+        if(!$magnifyingGlass)
+        {
+            throw new PSPInvalidOperationException('Goodness! It\'s so small! You\'ll need a magnifying glass of some kind...');
+        }
+
+        return $responseService->itemActionSuccess(<<<EOBOOK
+<em>(You know how on the back of a bag of chocolate chips, there's a recipe for Chocolate Chip Cookies? This is like that, but on the sticker on this Yellowy Lime. Oh, and also the recipe isn't for Chocolate Chip Cookies, because that would be weird. Oh, and ALSO-also, the print is just, like, absolutely and absurdly tiny. Thankfully your {$magnifyingGlass->getFullItemName()} is a good magnifying glass, and lets you make it all out.)</em>
+
+**Yellowy Key-y Lime Pie**
+* Yellowy Lime
+* Egg
+* Creamy Milk
+* Sugar
+* Butter
+* Graham Cracker
+EOBOOK
+);
+    }
+
+}
