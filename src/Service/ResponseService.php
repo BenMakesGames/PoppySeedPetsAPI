c5b253b191a107bdb516a6380868da05488cfa45
Ben H <ben@benmakesgames.com>
2023-09-20 12:06:56 -0400

    dramatically improving findUnreadForUser for certain users

--- src/Service/ResponseService.php
+++ src/Service/ResponseService.php
@@ -180,13 +180,21 @@ class ResponseService
     {
         $time = microtime(true);
 
+        // the `oneMonthAgo` clause has a HUGE impact on performance; we don't _really_ want to limit unread logs to
+        // any particular timeframe, but if you haven't played in a month... it's fine :P you'll figure it out.
         $logs = SimpleDb::createReadOnlyConnection()
             ->query(
                 'SELECT l.id,l.entry,l.icon,l.changes,l.interestingness
                 FROM pet_activity_log AS l
                 INNER JOIN pet AS p ON p.id = l.pet_id
-                WHERE p.owner_id = ? AND l.viewed = 0',
-                [ $user->getId() ]
+                WHERE
+                    p.owner_id = :userId
+                    AND l.viewed = 0
+                    AND l.created_on >= :oneMonthAgo',
+                [
+                    ':userId' => $user->getId(),
+                    ':oneMonthAgo' => (new \DateTimeImmutable())->modify('-1 month')->format('Y-m-d'),
+                ]
             )
             ->mapResults(
                 fn($id, $entry, $icon, $changes, $interestingness)
