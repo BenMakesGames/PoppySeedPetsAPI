3e86cb775d287e0a14d1d6136b752ef67bc29620
Ben H <ben@benmakesgames.com>
2023-12-13 10:02:46 -0500

    cooking buddy stat fix?

---
+++ migrations/2023/12/Version20231213100000.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231213100000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'correct Recipes Learned by Cooking Buddy stats';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("UPDATE user_stats SET VALUE=(SELECT COUNT(*) FROM known_recipes WHERE known_recipes.user_id=user_stats.user_id) WHERE user_stats.stat='Recipes Learned by Cooking Buddy'");
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
