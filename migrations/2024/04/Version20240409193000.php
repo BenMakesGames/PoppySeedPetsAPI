7d4223f048546742b736dcecb1487beaf1725cf8
Ben H <ben@benmakesgames.com>
2024-04-09 19:35:09 -0400

    S. Epée

---
+++ migrations/2024/04/Version20240409193000.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240409193000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'S. Epée';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<EOSQL
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`) VALUES (461,0,0,4,4,0,0.195,0.835,-38,0.56,0,0,0,0,0,0,"brawl",0,0,0,0,482,253,0,0,0,0,0,0,0,0,0,0,1,NULL,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1382,"S. Epée","\"Preliminary testing has indicated that S. Epée possesses a rudimentary form of telepathy, capable of projecting emotions and simple urges into the minds of those nearby. Subjects report feeling sensations of anger, hunger, and an overwhelming desire to use S. Epée in acts of violence. These effects are significantly diminished in the presence of lead, suggesting that S. Epée\'s psychic abilities are susceptible to standard psychic dampening measures.\"","tool/sword/epee-ess",NULL,461,NULL,0,NULL,NULL,0,8,NULL,NULL,NULL,0,NULL,0,10) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1473,1382,"an") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
