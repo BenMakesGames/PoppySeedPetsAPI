abddb5d45d3fcd8f3eb43361a6da2ac7bc69aa1b
Ben H <ben@benmakesgames.com>
2024-09-28 09:09:08 -0400

    for no reason, some new item groups

---
+++ migrations/2024/09/Version20240928090000.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240928090000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // Sword
+        $this->addSql(<<<EOSQL
+        INSERT INTO `item_group` (`id`, `name`, `is_craving`, `is_gift_shop`) VALUES (41, 'Sword', '0', '0')
+        ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 41 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE image LIKE 'tool/sword/%'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+
+        // Wand
+        $this->addSql(<<<EOSQL
+        INSERT INTO `item_group` (`id`, `name`, `is_craving`, `is_gift_shop`) VALUES (42, 'Wand', '0', '0')
+        ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 42 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE image LIKE 'tool/wand/%'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+
+        // Spear
+        $this->addSql(<<<EOSQL
+        INSERT INTO `item_group` (`id`, `name`, `is_craving`, `is_gift_shop`) VALUES (43, 'Spear', '0', '0')
+        ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 43 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE image LIKE 'tool/spear/%'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+
+        // Scroll
+        $this->addSql(<<<EOSQL
+        INSERT INTO `item_group` (`id`, `name`, `is_craving`, `is_gift_shop`) VALUES (44, 'Scroll', '0', '0')
+        ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 44 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE image LIKE 'scroll/%'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+
+        // Bag
+        $this->addSql(<<<EOSQL
+        INSERT INTO `item_group` (`id`, `name`, `is_craving`, `is_gift_shop`) VALUES (45, 'Bag', '0', '0')
+        ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 45 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE image LIKE 'bag/%'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+
+        $this->addSql(<<<EOSQL
+        INSERT INTO item_group_item (item_group_id,item_id)
+        SELECT 45 AS item_group_id, id AS item_id
+        FROM `item`
+        WHERE name LIKE '% Fertilizer'
+        ON DUPLICATE KEY UPDATE `item_group_id` = `item_group_id`;
+        EOSQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
