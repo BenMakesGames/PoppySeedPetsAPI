dd5aa8eb345ab94126fa8b1a5cd0dfff84987939
Ben H <ben@benmakesgames.com>
2024-10-24 20:14:29 -0400

    spice repository caching fix

--- src/Functions/SpiceRepository.php
+++ src/Functions/SpiceRepository.php
@@ -1,16 +1,23 @@
 <?php
-
 namespace App\Functions;
 
 use App\Entity\Spice;
+use App\Exceptions\PSPNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 
 class SpiceRepository
 {
-    public static function findOneByName(EntityManagerInterface $em, string $name): ?Spice
+    public static function findOneByName(EntityManagerInterface $em, string $spiceName): ?Spice
     {
-        return $em->getRepository(Spice::class)
-            ->findOneBy([ 'name' => $name ])
-            ->enableResultCache(24 * 60 * 60, CacheHelpers::getCacheItemName('SpiceRepository_FindOneByName_' . $name));
+        $spice = $em->getRepository(Spice::class)->createQueryBuilder('s')
+            ->where('s.name=:name')
+            ->setParameter('name', $spiceName)
+            ->getQuery()
+            ->enableResultCache(24 * 60 * 60, CacheHelpers::getCacheItemName('SpiceRepository_FindOneByName_' . $spiceName))
+            ->getOneOrNullResult();
+
+        if(!$spice) throw new PSPNotFoundException('There is no spice called ' . $spiceName . '.');
+
+        return $spice;
     }
 }
