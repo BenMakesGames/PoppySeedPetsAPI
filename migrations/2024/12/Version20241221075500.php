c6a73b492afad5d44d136a3e9dbed87f7ac82eee
Ben H <ben@benmakesgames.com>
2024-12-21 08:45:11 -0500

    philosopher's stone merit text tweak

--- migrations/2024/12/Version20241221075500.php
+++ migrations/2024/12/Version20241221075500.php
@@ -18,25 +18,25 @@ final class Version20241221075500 extends AbstractMigration
     {
         // Ichthyastra
         $this->addSql(<<<EOSQL
-        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (60, 'Ichthyastra', 'A third of the Fish that %pet.name% acquires will have a random spice applied, however %pet.name% can never again seek the Vesica Hydrargyrum.')
+        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (60, 'Ichthyastra', 'A third of the Fish that %pet.name% acquires will have a random spice applied, however %pet.name% will never again find a Vesica Hydrargyrum.')
         ON DUPLICATE KEY UPDATE `id` = `id`;
         EOSQL);
 
         // Metatron's Touch
         $this->addSql(<<<EOSQL
-        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (61, 'Metatron\'s Touch', 'When %pet.name% makes something with Firestone, a Rock is leftover, however %pet.name% can never again seek Metatron\'s Fire.')
+        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (61, 'Metatron\'s Touch', 'When %pet.name% makes something with Firestone, a Rock is leftover, however %pet.name% will never again find Metatron\'s Fire.')
         ON DUPLICATE KEY UPDATE `id` = `id`;
         EOSQL);
 
         // Manxome
         $this->addSql(<<<EOSQL
-        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (62, 'Manxome', 'If %pet.name% has less Dexterity than Stamina, it gets +1 Dexterity, otherwise it gets +1 Stamina. Regardless, %pet.name% can never again seek Earth\'s Egg.')
+        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (62, 'Manxome', 'If %pet.name% has less Dexterity than Stamina, it gets +1 Dexterity, otherwise it gets +1 Stamina. Regardless, %pet.name% will never again find Earth\'s Egg.')
         ON DUPLICATE KEY UPDATE `id` = `id`;
         EOSQL);
 
         // Lightning Reins
         $this->addSql(<<<EOSQL
-        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (63, 'Lightning Reins', 'Whenever %pet.name% collects Lightning in a Bottle, it also gets Quintessence, however %pet.name% can never again seek the Merkaba of Air.')
+        INSERT INTO `merit` (`id`, `name`, `description`) VALUES (63, 'Lightning Reins', 'Whenever %pet.name% collects Lightning in a Bottle, it also gets Quintessence, however %pet.name% will never again find a Merkaba of Air.')
         ON DUPLICATE KEY UPDATE `id` = `id`;
         EOSQL);
     }
