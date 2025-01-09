ea7fff383a11253df2444480d7b81b2c617b0fb2
Ben <ben@benmakesgames.com>
2025-01-09 16:09:15 -0500

    Added Chicken Smoothie, Goatlings, and Lorwolf to the "on the web" links people can include in their profiles

--- src/Enum/UserLinkWebsiteEnum.php
+++ src/Enum/UserLinkWebsiteEnum.php
@@ -6,17 +6,20 @@ class UserLinkWebsiteEnum
 {
     use Enum;
 
+    public const CHICKEN_SMOOTHIE = 'ChickenSmoothie';
     public const COHOST = 'Cohost';
     public const DEVIANT_ART = 'DeviantArt';
     public const FLIGHT_RISING = 'FlightRising';
     public const FUR_AFFINITY = 'FurAffinity';
     public const GAIA_ONLINE = 'GaiaOnline';
     public const GITHUB = 'GitHub';
+    public const GOATLINGS = 'Goatlings';
+    public const INSTAGRAM = 'Instagram';
+    public const LORWOLF = 'Lorwolf';
     public const MASTODON = 'Mastodon';
     public const NINTENDO = 'Nintendo';
     public const PIXEL_CATS_END = 'PixelCatsEnd';
     public const POPPY_SEED_PETS = 'PSP';
-    public const INSTAGRAM = 'Instagram';
     public const STEAM = 'Steam';
     public const TUMBLR = 'Tumblr';
     public const TWITCH = 'Twitch';
