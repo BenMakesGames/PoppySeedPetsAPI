6e2f1bda0d8068d8df82c25038dee29d0e737f9d
Ben <ben@benmakesgames.com>
2025-01-13 15:21:37 -0500

    Etalocŏhc - probably done! will test, later

---
+++ migrations/2025/01/Version20250113151500.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250113151500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // Etalocŏhc
+        $this->addSql(<<<EOSQL
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`) VALUES (490,0,0,0,4,0,0.65,0.78,27,0.6,0,0,0,0,0,0,NULL,0,0,0,0,1158,815,0,0,0,0,0,0,0,0,0,0,1,NULL,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- food effect
+        INSERT INTO item_food (`id`, `food`, `love`, `junk`, `alcohol`, `earthy`, `fruity`, `tannic`, `spicy`, `creamy`, `meaty`, `planty`, `fishy`, `floral`, `fatty`, `oniony`, `chemically`, `caffeine`, `psychedelic`, `granted_skill`, `chance_for_bonus_item`, `random_flavor`, `contains_tentacles`, `granted_status_effect`, `granted_status_effect_duration`, `is_candy`, `leftovers_id`, `bonus_item_group_id`) VALUES (516,3,6,2,0,2,0,0,0,1,0,0,0,0,0,0,0,0,0,NULL,NULL,0,0,NULL,NULL,1,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1430,"Etalocŏhc","This strange wand seems to resist, even reject chocolate! If you value your Chocolate Feather Bonnet, I wouldn\'t get it too close to one of these!","tool/wand/etalocohc",NULL,490,516,10,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1515,1430,"an") ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- item groups
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (42, 1430), (46, 1430);
+        EOSQL);
+
+        // Non-chocolate Feather Bonnet
+        $this->addSql(<<<EOSQL
+        -- hat
+        INSERT INTO item_hat (`id`, `head_x`, `head_y`, `head_angle`, `head_scale`, `head_angle_fixed`) VALUES (277,0.54,0.69,-10,0.7,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1431,"Non-chocolate Feather Bonnet",NULL,"hat/red-feather-bonnet",NULL,NULL,NULL,0,NULL,277,0,0,NULL,NULL,NULL,0,NULL,0,15) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1516,1431,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        // De-chocolated Meringue
+        $this->addSql(<<<EOSQL
+        -- hat
+        INSERT INTO item_hat (`id`, `head_x`, `head_y`, `head_angle`, `head_scale`, `head_angle_fixed`) VALUES (278,0.5,0.74,0,0.42,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- food effect
+        INSERT INTO item_food (`id`, `food`, `love`, `junk`, `alcohol`, `earthy`, `fruity`, `tannic`, `spicy`, `creamy`, `meaty`, `planty`, `fishy`, `floral`, `fatty`, `oniony`, `chemically`, `caffeine`, `psychedelic`, `granted_skill`, `chance_for_bonus_item`, `random_flavor`, `contains_tentacles`, `granted_status_effect`, `granted_status_effect_duration`, `is_candy`, `leftovers_id`, `bonus_item_group_id`) VALUES (517,2,5,1,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,NULL,NULL,0,0,NULL,NULL,1,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1432,"De-chocolated Meringue",NULL,"dessert/blue-meringue",NULL,NULL,517,6,NULL,278,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1517,1432,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- item groups
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (46, 1432);
+        EOSQL);
+
+        // Thaumatoxic Cookies
+        $this->addSql(<<<EOSQL
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1433,"Thaumatoxic Cookies",NULL,"dessert/cookies-thaumatoxic",NULL,NULL,NULL,0,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1518,1433,"some") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        // Odd Little Hammer
+        $this->addSql(<<<EOSQL
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`) VALUES (491,0,0,1,1,1,0.39,0.65,-29,0.64,0,0,0,1,0,0,NULL,0,0,0,0,NULL,NULL,0,0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1434,"Odd Little Hammer","Holding this hammer, you catch a faint whiff of chocolate.
+        
+        Odd.","tool/hammer/odd-little",NULL,491,NULL,0,NULL,NULL,0,1,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1519,1434,"an") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+        // Strawberry-covered Naner
+        $this->addSql(<<<EOSQL
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`) VALUES (492,0,0,0,2,0,0.485,0.8,0,0.6,2,0,0,0,0,0,NULL,0,0,0,0,1158,274,0,0,0,0,1,1,0,0,0,0,1,NULL,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- food effect
+        INSERT INTO item_food (`id`, `food`, `love`, `junk`, `alcohol`, `earthy`, `fruity`, `tannic`, `spicy`, `creamy`, `meaty`, `planty`, `fishy`, `floral`, `fatty`, `oniony`, `chemically`, `caffeine`, `psychedelic`, `granted_skill`, `chance_for_bonus_item`, `random_flavor`, `contains_tentacles`, `granted_status_effect`, `granted_status_effect_duration`, `is_candy`, `leftovers_id`, `bonus_item_group_id`) VALUES (518,10,10,1,0,0,4,0,0,0,0,1,0,0,0,0,0,0,0,NULL,NULL,0,0,NULL,NULL,1,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1435,"Strawberry-covered Naner","Strawberries? In Poppy Seed Pets? Most unusual! Some crazy-powerful magic must have been involved in the creation of this item...","dessert/naner-strawberry-covered",NULL,492,518,20,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1520,1435,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- item groups
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (46, 1435);
+        EOSQL);
+
+        // Mildly Thaumatoxic Top Hat
+        $this->addSql(<<<EOSQL
+        -- hat
+        INSERT INTO item_hat (`id`, `head_x`, `head_y`, `head_angle`, `head_scale`, `head_angle_fixed`) VALUES (279,0.49,0.765,0,0.55,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1436,"Mildly Thaumatoxic Top Hat","It\'s only _mildly_ thaumatoxic!
+        
+        <small>_This hat is known to the State of California to cause reproductive harm._</small>","hat/top-hat-thaumatoxic",NULL,NULL,NULL,0,NULL,279,720,0,NULL,NULL,NULL,0,NULL,0,15) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1521,1436,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
