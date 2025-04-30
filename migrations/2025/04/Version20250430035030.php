fcad459dba2343954bb2d7d1cdab872ec2e5de0e
Ben H <ben@benmakesgames.com>
2025-04-29 23:54:25 -0400

    graveling hook update

---
+++ migrations/2025/04/Version20250430035030.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250430035030 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // graveling hook
+        $this->addSql(<<<EOSQL
+        UPDATE `item_tool` SET `brawl` = '3', `mining` = '3' WHERE `item_tool`.`id` = 332; 
+        EOSQL);
+
+        // graveling hook enchantment
+        $this->addSql(<<<EOSQL
+        UPDATE `item_tool` SET `brawl` = 0, `mining` = '3' WHERE `item_tool`.`id` = 333; 
+        EOSQL);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
