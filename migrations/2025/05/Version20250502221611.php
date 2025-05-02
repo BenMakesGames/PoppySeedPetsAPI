85cb7ba8e8a546ed3266d3c269c3d47c3d14836a
Ben H <ben@benmakesgames.com>
2025-05-02 18:16:57 -0400

    migration

---
+++ migrations/2025/05/Version20250502221611.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250502221611 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Plastic Bottles';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<EOSQL
+        -- hat
+        INSERT INTO item_hat (`id`, `head_x`, `head_y`, `head_angle`, `head_scale`, `head_angle_fixed`) VALUES (290,0.42,0.845,-11,0.44,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1457,"Plastic Bottle","Plastic?!? What\'s wrong with Glass??? \\*sigh\\*","box/bottle","[[\"Open\",\"plasticBottle\\/#\\/open\"]]",NULL,NULL,0,NULL,290,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1542,1457,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
