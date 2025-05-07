1d5feb2eb6bb99e5b4471385e404c62e78d19d5c
Ben H <ben@benmakesgames.com>
2025-05-07 07:58:10 -0400

    Eiri Personal level-down

--- migrations/2025/05/Version20250507115704.php
+++ migrations/2025/05/Version20250507115704.php
@@ -16,33 +16,23 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20250507033029 extends AbstractMigration
+final class Version20250507115704 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'set Eiri Persona level to level of most-recent Cardea';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql(<<<EOSQL
-        -- hat
-        INSERT INTO item_hat (`id`, `head_x`, `head_y`, `head_angle`, `head_scale`, `head_angle_fixed`) VALUES (293,0.525,0.83,0,0.62,0) ON DUPLICATE KEY UPDATE `id` = `id`;
-        
-        -- the item itself!
-        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1464,"Merchant\'s Other Cap",NULL,"hat/merchants-2",NULL,NULL,NULL,0,NULL,293,105,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
-        
-        -- grammar
-        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1549,1464,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        UPDATE `monster_of_the_week` SET level=(
+            SELECT q.level FROM (SELECT level FROM monster_of_the_week WHERE monster='Cardea' ORDER BY id DESC LIMIT 1) AS q)
+        WHERE monster='Eiri Persona';
         EOSQL);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
     }
 }
