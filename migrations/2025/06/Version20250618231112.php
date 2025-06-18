6aaa5c4807c1d64b8f8e69679a1ae11c298d4906
Ben H <ben@benmakesgames.com>
2025-06-18 16:31:05 -0700

    gulab jamun fix

--- migrations/2025/06/Version20250618231112.php
+++ migrations/2025/06/Version20250618231112.php
@@ -30,15 +30,14 @@ final class Version20250618231112 extends AbstractMigration
         INSERT INTO item_food (`id`, `food`, `love`, `junk`, `alcohol`, `earthy`, `fruity`, `tannic`, `spicy`, `creamy`, `meaty`, `planty`, `fishy`, `floral`, `fatty`, `oniony`, `chemically`, `caffeine`, `psychedelic`, `granted_skill`, `chance_for_bonus_item`, `random_flavor`, `contains_tentacles`, `granted_status_effect`, `granted_status_effect_duration`, `is_candy`, `leftovers_id`, `bonus_item_group_id`) VALUES (523,4,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL,NULL,0,0,NULL,NULL,0,NULL,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
         
         -- the item itself!
-        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1466,"Gulab Jamun","The tricky part is simmering the milk down into solids, but once that\'s done you mix it up with some wheat flour and butter, fry it up, and soak it in sugar syrup! Bam - done!","dessert/gulab-jamun",NULL,NULL,523,9,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1476,"Gulab Jamun","The tricky part is simmering the milk down into solids, but once that\'s done you mix it up with some wheat flour and butter, fry it up, and soak it in sugar syrup! Bam - done!","dessert/gulab-jamun",NULL,NULL,523,9,NULL,NULL,0,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
         
         -- grammar
-        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1551,1466,"some") ON DUPLICATE KEY UPDATE `id` = `id`;
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1551,1476,"some") ON DUPLICATE KEY UPDATE `id` = `id`;
         
         -- item groups
-        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (11, 1466), (28, 1466), (46, 1466);
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (11, 1476), (28, 1476), (46, 1476);
         EOSQL);
-
     }
 
     public function down(Schema $schema): void
