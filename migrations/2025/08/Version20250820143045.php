e7230919a5554e86899a9dcd08ce7bb0359460e8
nibkind <nibblerkind@gmail.com>
2025-08-20 07:05:44 -1000

    Create Version20250820143045.php

---
+++ migrations/2025/08/Version20250820143045.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250820143045 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add fatty & creamy flavors to Gulab Jamun';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // Add fatty & creamy flavors to Gulab Jamun
+        $this->addSql("UPDATE item_food SET fatty = 1, creamy = 1 WHERE id = 1476");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // Revert Gulab Jamun flavor change
+        $this->addSql("UPDATE item_food SET fatty = 0, creamy = 0 WHERE id = 1476");
+    }
+}
