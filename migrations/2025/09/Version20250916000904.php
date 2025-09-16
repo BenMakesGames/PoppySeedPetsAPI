bdf95bf347c922ea20ac518a3c00e65e5aef4b58
Ben H <ben@benmakesgames.com>
2025-09-15 17:10:25 -0700

    forge (WIP)

---
+++ migrations/2025/09/Version20250916000904.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250916000904 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add Forge Blueprint';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'EOSQL'
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1483,"Forge Blueprint","This blueprint shows you and your pets how to build a functioning forge from nothing more than a piece of Iron and a Heavy Hammer! (Apparently that\'s all you need!)","note/blueprint-forge","[[\"Read\",\"forgeBlueprint\",\"page\"]]",NULL,NULL,0,NULL,NULL,90,0,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+        
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1567,1483,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+        EOSQL);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
