5d64cf29d6b4ecfc1e4d7ac64872b01981e00f57
Ben Hendel-Doying <ben@benmakesgames.com>
2025-09-17 17:43:22 -0400

    fix when preparing >= 10 items at once (#139)

--- src/Controller/Inventory/CookAndCombineController.php
+++ src/Controller/Inventory/CookAndCombineController.php
@@ -147,10 +147,10 @@ class CookAndCombineController
 
         if($totalQuantity < 4)
             $exclaim = '.';
-        else if($totalQuantity < 10 || strpos($results->recipe['ingredients'], ',') === false)
+        else if($totalQuantity < 10 || strpos($results->recipe->ingredients, ',') === false)
             $exclaim = '!';
         else
-            $exclaim = '! (' . $rng->rngNextFromArray([ 'Dang!', 'Wow!', 'Incredible...', 'So cook! Very meal!', 'A veritable feast!', 'Such skill!' ]) . ')';
+            $exclaim = '! (' . $rng->rngNextFromArray([ 'Dang!', 'Wow!', 'Wowie-zowie!', 'Incredible...', 'So cook! Very meal!', 'A veritable feast!', 'Such skill! Such panache!', 'When did you become so powerful?!' ]) . ')';
 
         $responseService->addFlashMessage('You prepared ' . ArrayFunctions::list_nice_quantities($qList) . $exclaim);
 
