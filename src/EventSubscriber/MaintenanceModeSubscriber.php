e239580a9e63432c5994f3b491f6db64c44fd516
Ben H <ben@benmakesgames.com>
2025-09-20 09:08:24 -0700

    license + stop propagation after maintenance mode

--- src/EventSubscriber/MaintenanceModeSubscriber.php
+++ src/EventSubscriber/MaintenanceModeSubscriber.php
@@ -1,6 +1,16 @@
 <?php
 namespace App\EventSubscriber;
 
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -10,10 +20,6 @@ class MaintenanceModeSubscriber implements EventSubscriberInterface
 {
     public function onKernelRequest(RequestEvent $event)
     {
-        $request = $event->getRequest();
-        if ($request->getPathInfo() === '/heartbeat') {
-            return;
-        }
         if (isset($_SERVER['APP_MAINTENANCE']) && $_SERVER['APP_MAINTENANCE']) {
             $response = new JsonResponse([
                 'success' => false,
@@ -21,6 +27,7 @@ class MaintenanceModeSubscriber implements EventSubscriberInterface
             ]);
 
             $event->setResponse($response);
+            $event->stopPropagation();
         }
     }
 
