aac5bfa7525974130c4a8054172489990515af75
Ben H <ben@benmakesgames.com>
2025-10-03 21:02:33 -0700

    50 max hours of flower power; not 10!

--- src/Entity/Beehive.php
+++ src/Entity/Beehive.php
@@ -112,7 +112,7 @@ class Beehive
 
     public function getMaxFlowerPower(): float
     {
-        return log($this->getWorkers()) * 10;
+        return log($this->getWorkers()) * 50;
     }
 
     public function getFlowerPower(): float
