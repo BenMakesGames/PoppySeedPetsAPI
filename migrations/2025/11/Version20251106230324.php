35f3e0e826d3c183f655f33e7627a251b0a1645b
Ben H <ben@benmakesgames.com>
2025-11-06 19:38:27 -0800

    added license to migration

--- migrations/2025/11/Version20251106230324.php
+++ migrations/2025/11/Version20251106230324.php
@@ -1,15 +1,21 @@
 <?php
-
 declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20251106230324 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,14 +25,12 @@ final class Version20251106230324 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD last_performed_quality_time DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql("UPDATE `user` SET last_performed_quality_time = DATE_SUB(NOW(), INTERVAL '1' DAY);");
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP last_performed_quality_time');
     }
 }
