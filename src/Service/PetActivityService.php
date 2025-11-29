15a622fc47eb7f8b6922ebc20d96e0d595ea2749
Ben H <ben@benmakesgames.com>
2025-11-28 21:18:21 -0800

    another refactor to PetActivityService

--- src/Service/PetActivityService.php
+++ src/Service/PetActivityService.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
  * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
  */
 
-
 namespace App\Service;
 
 use App\Entity\LunchboxItem;
@@ -29,7 +28,6 @@ use App\Enum\PetBadgeEnum;
 use App\Enum\PetSkillEnum;
 use App\Enum\StatusEffectEnum;
 use App\Enum\UnlockableFeatureEnum;
-use App\Enum\UserStat;
 use App\Functions\ActivityHelpers;
 use App\Functions\ArrayFunctions;
 use App\Functions\CalendarFunctions;
@@ -45,7 +43,6 @@ use App\Model\FoodWithSpice;
 use App\Model\PetChanges;
 use App\Model\PetChangesSummary;
 use App\Service\PetActivity\CachingMeritAdventureService;
-use App\Service\PetActivity\DokiDokiService;
 use App\Service\PetActivity\DreamingAndDaydreamingService;
 use App\Service\PetActivity\EatingService;
 use App\Service\PetActivity\FatedAdventureService;
@@ -55,22 +52,12 @@ use App\Service\PetActivity\GivingTreeGatheringService;
 use App\Service\PetActivity\GuildService;
 use App\Service\PetActivity\Holiday\HuntTurkeyDragon;
 use App\Service\PetActivity\IPetActivity;
-use App\Service\PetActivity\JumpRopeService;
-use App\Service\PetActivity\KappaService;
 use App\Service\PetActivity\LetterService;
-use App\Service\PetActivity\MortarOrPestleService;
 use App\Service\PetActivity\PetCleaningSelfService;
 use App\Service\PetActivity\PetSummonedAwayService;
-use App\Service\PetActivity\PhilosophersStoneService;
 use App\Service\PetActivity\PoopingService;
 use App\Service\PetActivity\PregnancyService;
-use App\Service\PetActivity\SpecialLocations\Caerbannog;
-use App\Service\PetActivity\SpecialLocations\ChocolateMansion;
-use App\Service\PetActivity\SpecialLocations\FructalPlaneService;
-use App\Service\PetActivity\SpecialLocations\HeartDimensionService;
-use App\Service\PetActivity\SpecialLocations\LostInTownService;
-use App\Service\PetActivity\SportsBallActivityService;
-use App\Service\PetActivity\TreasureMapService;
+use App\Service\PetActivity\ToolAdventures;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 
@@ -80,15 +67,12 @@ class PetActivityService
         private readonly Clock $clock,
         private readonly EntityManagerInterface $em,
         private readonly ResponseService $responseService,
-        private readonly HeartDimensionService $heartDimensionService,
-        private readonly UserStatsService $userStatsRepository,
         private readonly GenericAdventureService $genericAdventureService,
         private readonly PetSummonedAwayService $petSummonedAwayService,
         private readonly PoopingService $poopingService,
         private readonly GivingTreeGatheringService $givingTreeGatheringService,
         private readonly PregnancyService $pregnancyService,
         private readonly IRandom $rng,
-        private readonly ChocolateMansion $chocolateMansion,
         private readonly PetExperienceService $petExperienceService,
         private readonly DreamingAndDaydreamingService $dreamingAndDaydreamingService,
         private readonly EatingService $eatingService,
@@ -96,22 +80,13 @@ class PetActivityService
         private readonly GuildService $guildService,
         private readonly InventoryService $inventoryService,
         private readonly LetterService $letterService,
-        private readonly Caerbannog $caerbannog,
-        private readonly TreasureMapService $treasureMapService,
         private readonly HouseSimService $houseSimService,
         private readonly CravingService $cravingService,
-        private readonly PhilosophersStoneService $philosophersStoneService,
-        private readonly KappaService $kappaService,
         private readonly FatedAdventureService $fatedAdventureService,
         private readonly PetCleaningSelfService $petCleaningSelfService,
         private readonly CachingMeritAdventureService $cachingMeritAdventureService,
-        private readonly JumpRopeService $jumpRopeService,
-        private readonly DokiDokiService $dokiDokiService,
-        private readonly LostInTownService $lostInTownService,
-        private readonly FructalPlaneService $fructalPlaneService,
-        private readonly MortarOrPestleService $mortarOrPestleService,
-        private readonly SportsBallActivityService $sportsBallActivityService,
         private readonly HuntTurkeyDragon $huntTurkeyDragon,
+        private readonly ToolAdventures $toolAdventures,
         /**
          * @var iterable<IPetActivity>
          */
@@ -490,7 +465,7 @@ class PetActivityService
         if($this->discoverNewFeature($pet))
             return;
 
-        if($this->considerToolsWhichLeadToAdventure($petWithSkills))
+        if($this->toolAdventures->maybeDoToolAdventure($petWithSkills))
             return;
 
         if($this->rng->rngNextInt(1, $hasEventPersonality ? 48 : 50) === 1)
@@ -532,202 +507,6 @@ class PetActivityService
         $activityLog = $activity($petWithSkills);
 
         $activityLog->setChanges($changes->compare($pet));
-    }
-
-    private function considerToolsWhichLeadToAdventure(ComputedPetSkills $petWithSkills): bool
-    {
-        $pet = $petWithSkills->getPet();
-        $tool = $pet->getTool();
-
-        if(!$tool)
-            return false;
-
-        switch($tool->getItem()->getName())
-        {
-            case '"Gold" Idol':
-                $this->treasureMapService->doGoldIdol($pet);
-                return true;
-
-            case '5-leaf Clover':
-                $this->treasureMapService->doLeprechaun($petWithSkills);
-                return true;
-
-            case 'Aubergine Commander':
-                if($this->rng->rngNextInt(1, 80) === 1)
-                {
-                    $this->treasureMapService->doEggplantCurse($pet);
-                    return true;
-                }
-                break;
-
-            case 'Carrot Key':
-                $this->caerbannog->adventure($petWithSkills);
-                return true;
-
-            case 'Ceremony of Fire':
-                if($this->rng->rngNextInt(1, 20) == 1)
-                {
-                    $this->philosophersStoneService->seekVesicaHydrargyrum($petWithSkills);
-                    return true;
-                }
-                break;
-
-            case 'Cetgueli\'s Treasure Map':
-                $this->treasureMapService->doCetguelisTreasureMap($petWithSkills);
-                return true;
-
-            case 'Chocolate Key':
-                $this->chocolateMansion->adventure($petWithSkills);
-                return true;
-
-            case 'Cucumber':
-                $this->kappaService->doHuntKappa($petWithSkills);
-                return true;
-
-            case 'Diffie-H Key':
-                $this->treasureMapService->doUseDiffieHKey($pet);
-                return true;
-
-            case 'Fimbulvetr':
-                if($this->rng->rngNextInt(1, 20) == 1)
-                {
-                    $this->philosophersStoneService->seekMetatronsFire($petWithSkills);
-                    return true;
-                }
-                break;
-
-            case 'Fruit Fly on a String':
-                $this->treasureMapService->doFruitHunting($pet);
-                return true;
-
-            case 'Green Sportsball Ball':
-                $this->sportsBallActivityService->doGreenSportsballBall($petWithSkills);
-                return true;
-
-            case 'Heartstone':
-                if(!$this->heartDimensionService->canAdventure($pet))
-                {
-                    $this->heartDimensionService->notEnoughAffectionAdventure($pet);
-                    return true;
-                }
-                else if($this->rng->rngNextInt(1, 100) <= $this->heartDimensionService->chanceOfHeartDimensionAdventure($pet))
-                {
-                    $this->heartDimensionService->adventure($petWithSkills);
-                    return true;
-                }
-
-                break;
-
-            case 'Jump Rope':
-                if($this->rng->rngNextInt(1, 4) == 1)
-                {
-                    $this->jumpRopeService->adventure($petWithSkills);
-                    return true;
-                }
-                break;
-
-            case 'Large Radish':
-                $this->dokiDokiService->adventure($petWithSkills);
-                return true;
-
-            case 'Mortar or Pestle':
-                if($this->mortarOrPestleService->findTheOtherBit($petWithSkills->getPet()))
-                    return true;
-                break;
-
-            case 'Orange Sportsball Ball':
-                $this->sportsBallActivityService->doOrangeSportsballBall($petWithSkills);
-                return true;
-
-            case 'Saucepan':
-                if($this->rng->rngNextInt(1, 10) === 1)
-                {
-                    $this->treasureMapService->doCookSomething($pet);
-                    return true;
-                }
-
-                break;
-
-            case 'Shirikodama':
-                $this->kappaService->doReturnShirikodama($petWithSkills);
-                return true;
-
-            case 'Skewered Marshmallow':
-                if($this->rng->rngNextInt(1, 10) == 1)
-                {
-                    $this->treasureMapService->doToastSkeweredMarshmallow($pet);
-                    return true;
-                }
-                break;
-
-            case 'Snickerblade':
-                if($this->rng->rngNextInt(1, 20) == 1)
-                {
-                    $this->philosophersStoneService->seekEarthsEgg($petWithSkills);
-                    return true;
-                }
-                break;
-
-            case 'Sportsball Oar':
-                $this->sportsBallActivityService->doSportsballOar($petWithSkills);
-                return true;
-
-            case 'Sportsball Pin':
-                $this->sportsBallActivityService->doSportsballPin($petWithSkills);
-                return true;
-
-            case 'Winged Key':
-                $this->treasureMapService->doAbundantiasVault($pet);
-                return true;
-
-            case 'Woher Cuán Nani-nani':
-                $this->lostInTownService->adventure($petWithSkills);
-                return true;
-
-            case 'Silver Keyblade':
-            case 'Gold Keyblade':
-                if($pet->getFood() > 0 && $this->rng->rngNextInt(1, 10) === 1)
-                {
-                    $this->treasureMapService->doKeybladeTower($petWithSkills);
-                    return true;
-                }
-
-                break;
-
-            case 'Rainbow Dolphin Plushy':
-            case 'Sneqo Plushy':
-            case 'Bulbun Plushy':
-            case 'Peacock Plushy':
-            case 'Phoenix Plushy':
-            case '"Roy" Plushy':
-                if($this->rng->rngNextInt(1, 6) === 1 || $this->userStatsRepository->getStatValue($pet->getOwner(), UserStat::TradedWithTheFluffmonger) === 0)
-                {
-                    $this->treasureMapService->doFluffmongerTrade($pet);
-                    return true;
-                }
-
-                break;
-        }
-
-        if($tool->getEnchantment())
-        {
-            switch($tool->getEnchantment()->getName())
-            {
-                case 'Searing':
-                    if($this->rng->rngNextInt(1, 20) == 1)
-                    {
-                        if($this->philosophersStoneService->seekMerkabaOfAir($petWithSkills))
-                            return true;
-                    }
-                    break;
-
-                case 'Gooder':
-                    $this->fructalPlaneService->adventure($petWithSkills);
-                    return true;
-            }
-        }
-
-        return false;
     }
 
     private function doNothing(ComputedPetSkills $petWithSkills): PetActivityLog
