29d11cc29ab3ef7d0cc454626948a6707fce2330
Ben H <ben@benmakesgames.com>
2026-01-03 08:40:39 -0800

    c'est une fix

---
+++ migrations/2026/01/Version20260103163949.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Poppy Seed Pets API.
+ *
+ * The Poppy Seed Pets API is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The Poppy Seed Pets API is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with The Poppy Seed Pets API. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260103163949 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'fix treble scale aura';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("UPDATE `aura` SET `image` = 'treble-scale' WHERE `aura`.`id` = 40;");
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
