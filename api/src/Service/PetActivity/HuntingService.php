5030b229e54dde6955a8947a9ca7c8d61fb33d37
Vermidia <vermidiathesnake@gmail.com>
2026-03-24 22:38:11 -0500

    More guild stuff part 2 - Satyr party

--- api/src/Service/PetActivity/HuntingService.php
+++ api/src/Service/PetActivity/HuntingService.php
@@ -1227,6 +1227,22 @@ class HuntingService implements IPetActivity
             $this->petExperienceService->gainExp($pet, 1, [ PetSkillEnum::Music ], $activityLog);
             $this->petExperienceService->spendTime($pet, $this->rng->rngNextInt(45, 60), PetActivityStatEnum::HUNT, true);
         }
+        else if ($pet->isInGuild(GuildEnum::GizubisGarden) || $pet->getAlcohol() > 8) // drunk enough pets can also party!
+        {
+            $activityLog = PetActivityLogFactory::createUnreadLog($this->em, $pet, '%pet:' . $pet->getId() . '.name% encountered a Satyr, who invited them to party! The Satyr danced with ' . $pet->getName() . ' and shared some gifts before leaving in peace.')
+                ->setIcon('icons/activity-logs/drunk-satyr')
+                ->addTags(PetActivityLogTagHelpers::findByNames($this->em, $pet->isInGuild(GuildEnum::GizubisGarden) ? ['Fae-kind', 'Guild'] : ['Fae-kind']))
+            ;
+            $pet->increaseEsteem(1);
+            $this->inventoryService->petCollectsItem('Blackberry Wine', $pet, 'Gifts for ' . $pet->getName() . ', from a Satyr.', $activityLog);
+
+            if ($this->rng->rngNextInt(1, 5) === 1)
+                $this->inventoryService->petCollectsItem('Blackberry Wine', $pet, 'Gifts for ' . $pet->getName() . ', from a Satyr.', $activityLog);
+            else
+                $this->inventoryService->petCollectsItem('Plain Yogurt', $pet, 'Gifts for ' . $pet->getName() . ', from a Satyr.', $activityLog);
+
+            $this->petExperienceService->spendTime($pet, $this->rng->rngNextInt(45, 60), PetActivityStatEnum::HUNT, true);
+        }
         else if($musicSkill > $brawlRoll)
         {
             if($pet->hasMerit(MeritEnum::SOOTHING_VOICE))
